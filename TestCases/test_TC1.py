60fe5ab67f19558323ef7363fd716b510a625d2f
vihithag <47211020+vihithag@users.noreply.github.com>
2020-09-21 18:32:18 +0530

    recreated the project again

--- TestCases/test_TC1.py
+++ TestCases/test_TC1.py
@@ -18,9 +18,11 @@ def test_fail(environment_setup):
     uname.send_keys("Admin")
     pname = browser.find_element_by_name("txtPassword")
     pname.send_keys("admin123")
+    Submitbutton = browser.find_element_by_name("Submit")
+    Submitbutton.click()
 
 def test_pass(environment_setup):
-    uname = browser.find_element_by_name("userName")
-    uname.send_keys("test")
-    pname = browser.find_element_by_name("password")
-    pname.send_keys("test")
+    uname = browser.find_element_by_name("txtUsername")
+    uname.send_keys("Admin")
+    forgot = browser.find_element_by_link_text("Forgot your password?")
+    forgot.click()
